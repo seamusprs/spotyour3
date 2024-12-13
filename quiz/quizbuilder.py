8929e1e7e5cd0abca55ac47b977c3e4cd8f5c1e3
jasonsuwito <jasonns@student.ubc.ca>
2024-12-12 16:41:14 -0800

    added try except statements in quizbuilder

--- quiz/quizbuilder.py
+++ quiz/quizbuilder.py
@@ -41,11 +41,23 @@ class Question:
         
     def getdata(self, num_song = 4):
         """Builds a list of songs randomly selected from the playlist, used as options when generating questions."""
-        idx = random.sample(range(self.playlist.length), num_song)
-        song_list = []
-        for i in idx:
-            song_list.append(rd.Song(self.playlist, id=i))
-        return song_list
+        try:
+            if num_song > self.playlist.length:
+                raise ValueError("Number of song request is more than the size of the playlist")
+            idx = random.sample(range(self.playlist.length), num_song)
+            song_list = []
+            for i in idx:
+                try:
+                    song_list.append(rd.Song(self.playlist, id=i))
+                except Exception as ex:
+                    raise InvalidPlaylist(f"Unable to access song data: {ex}")
+            return song_list
+        except ValueError as ex:
+            raise ex
+        except InvalidPlaylist as ex:
+            raise ex
+        except Exception as ex:
+            raise RuntimeError(f"Error in 'getdata': {ex}")
     
     def makequestion(self, question_type):
         """Takes the desired question type as an argument, calls the appropriate question type function,
@@ -93,68 +105,88 @@ class QuestionBuilder(Question):
     """
     def artist_question(self):
         """Builds a question identifying a song's artist."""
-        data = self.getdata()
-        song = random.choice(data)
-        question = f"Which artist performs the song '{song.track}'?"
-        options = []
-        options.append(song.artist)
-        for i in data:
-            if i.artist != song.artist:
-                options.append(i.artist)
-        random.shuffle(options)
-        return question, options, song.artist
+        try:
+            data = self.getdata()
+            song = random.choice(data)
+            question = f"Which artist performs the song '{song.track}'?"
+            options = []
+            options.append(song.artist)
+            for i in data:
+                if i.artist != song.artist:
+                    options.append(i.artist)
+            random.shuffle(options)
+            return question, options, song.artist
+        except Exception as ex:
+            raise RuntimeError(f"Error in 'age_question': {ex}")
     
     def label_question(self):
         """Builds a question identifying a song's record label."""
-        data = self.getdata()
-        song = random.choice(data)
-        question = f"Which label publishes the song '{song.track}'?"
-        options = []
-        options.append(song.label)
-        for i in data:
-            if i.label != song.label:
-                options.append(i.label)
-        random.shuffle(options)
-        return question, options, song.label
+        try:
+            data = self.getdata()
+            song = random.choice(data)
+            question = f"Which label publishes the song '{song.track}'?"
+            options = []
+            options.append(song.label)
+            for i in data:
+                if i.label != song.label:
+                    options.append(i.label)
+            random.shuffle(options)
+            return question, options, song.label
+        except Exception as ex:
+            raise RuntimeError(f"Error in 'label_question': {ex}")
     
     def length_question(self):
         """Builds a question comparing song lengths."""
-        data = self.getdata()
-        question = f"Which of these songs has the longest duration?"
-        temp_options = []
-        options = []
-        for i in data:
-            temp_options.append((i.track, i.duration))
-        solution = max(temp_options, key=lambda x: x[1])[0]
-        for j in temp_options:
-            options.append(j[0])
-        random.shuffle(options)
-        return question, options, solution
+        try:
+            data = self.getdata()
+            question = f"Which of these songs has the longest duration?"
+            temp_options = []
+            options = []
+            for i in data:
+                temp_options.append((i.track, i.duration))
+            solution = max(temp_options, key=lambda x: x[1])[0]
+            for j in temp_options:
+                options.append(j[0])
+            random.shuffle(options)
+            return question, options, solution
+        except Exception as ex:
+            raise RuntimeError(f"Error in 'length_question': {ex}")
     
     def age_question(self):
         """Builds a question comparing song release dates."""
-        data = self.getdata()
-        question = f"Which of these songs is the oldest?"
-        temp_options = []
-        options = []
-        for i in data:
-            temp_options.append((i.track, i.release))
-        solution = min(temp_options, key=lambda x: x[1])[0]
-        for j in temp_options:
-            options.append(j[0])
-        random.shuffle(options)
-        return question, options, solution
+        try:
+            data = self.getdata()
+            question = f"Which of these songs is the oldest?"
+            temp_options = []
+            options = []
+            for i in data:
+                temp_options.append((i.track, i.release))
+            solution = min(temp_options, key=lambda x: x[1])[0]
+            for j in temp_options:
+                options.append(j[0])
+            random.shuffle(options)
+            return question, options, solution
+        except Exception as ex:
+            raise RuntimeError(f"Error in 'age_question': {ex}")
     
     def tempo_question(self):
         """Builds a question comparing song tempos (BPM)."""
-        data = self.getdata()
-        question = f"Which of these songs has the slowest tempo?"
-        temp_options = []
-        options = []
-        for i in data:
-            temp_options.append((i.track, i.tempo))
-        solution = min(temp_options, key=lambda x: x[1])[0]
-        for j in temp_options:
-            options.append(j[0])
-        random.shuffle(options)
-        return question, options, solution
+        try:
+            data = self.getdata()
+            question = f"Which of these songs has the slowest tempo?"
+            temp_options = []
+            options = []
+            for i in data:
+                temp_options.append((i.track, i.tempo))
+            solution = min(temp_options, key=lambda x: x[1])[0]
+            for j in temp_options:
+                options.append(j[0])
+            random.shuffle(options)
+            return question, options, solution
+        except Exception as ex:
+            raise RuntimeError(f"Error in 'tempo_question': {ex}")
+
+class InvalidPlaylist(Exception):
+    def __init__(self, message="Playlist is invalid"):
+        self.message = message
+        super().__init__(self.message)
